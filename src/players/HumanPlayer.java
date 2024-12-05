c6417fca2d3b8d3e0b915887a41d2891ab52a58e
Miroslav Plavec <miroslav.plav@gmail.com>
2024-12-05 17:28:42 +0100

    I added method makeBet which read input from console and store user bet in current round

--- src/players/HumanPlayer.java
+++ src/players/HumanPlayer.java
@@ -1,12 +1,41 @@
 package players;
 
+import cards.Card;
+
+import java.util.Scanner;
+
 public class HumanPlayer extends Player{
+    private final String cardString = name + "`s cards are:";
     private int money;
+    private int bet;
 
     public HumanPlayer(String name, int money) {
         super(name);
         this.money = money;
+        bet = 0;
     }
 
+    public void makeBet(){
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println(name + " please place your bet. Your current balance is " + money + " €.");
+            bet = Integer.parseInt(scanner.nextLine());
+        }while (!checkBet());
 
+    }
+
+    private boolean checkBet(){
+        if(bet <= money && bet>0) return true;
+        System.out.println("Your bet must be in range 1 to " + money + " €.");
+        return false;
+    }
+
+    @Override
+    public void showCards() {
+        System.out.print("%s ".formatted(cardString));
+        for(Card card : cards){
+            System.out.print("%s ".formatted(card));
+        }
+        System.out.println();
+    }
 }
